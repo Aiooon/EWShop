7e267494a2e094d0d7d738003413faf08c6707ae
ZYao <1307526550@qq.com>
2021-06-12 22:24:54 +0800

    添加 banner 和 推荐商品

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,20 +3,52 @@
         <nav-bar>
             <template v-slot:default>图书商城</template>
         </nav-bar>
+        <div class="banner">
+            <img src="~assets/images/1.png" alt="">
+        </div>
+        <!-- 向子组件传数据 -->
+        <recommend-view :recommends="recommends"></recommend-view>
     </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar.vue'
+import NavBar from 'components/common/navbar/NavBar'
+import RecommendView from './ChildComps/RecommendView'
+import { getHomeAllData } from 'network/home'
+import { ref, reactive, onMounted } from 'vue'
 
 export default {
     name: 'Home',
+
+    setup () {
+        const recommends = ref([]);
+
+        onMounted( ()=>{
+            // console.log(recommends);
+            getHomeAllData().then(res=>{
+                console.log(res);
+                recommends.value = res.goods.data;
+            })
+        })
+
+        return {
+            recommends
+        }
+    },
+
     components: {
-        NavBar 
+        NavBar,
+        RecommendView,
     }
 }
 </script>
 
+        RecommendView
 <style scoped>
+.banner img{
+    width: 100%;
+    height: auto;
+    margin-top: 45px;
+}
 
 </style>
