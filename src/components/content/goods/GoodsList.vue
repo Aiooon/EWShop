4702519400bc421b18d8dc96f1b13440f2316ee6
ZYao <1307526550@qq.com>
2021-06-13 22:46:31 +0800

    新建商品列表组件

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="goods">
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+        <goods-list-item></goods-list-item>
+    </div>
+</template>
+
+<script>
+import GoodsListItem from './GoodsListItem'
+export default {
+    name: "GoodsList",
+    components: {
+        GoodsListItem
+    }
+}
+</script>
+
+<style scoped>
+
+.goods {
+    display: flex;    /* 弹性盒 */
+    flex-wrap: wrap;  /* 自动换行 */
+    justify-content: space-around;  /* 四周环绕 */
+    padding: 10px;
+}
+
+</style>
