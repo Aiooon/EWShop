5b6df6096ec545cbd9567a87f7563c7a69d6638b
ZYao <1307526550@qq.com>
2021-06-15 22:37:02 +0800

    添加下拉加载数据和滚动(better-scroll)

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,18 +1,27 @@
 <template>
-    <div>
+    <div id="home">
         <nav-bar>
             <template v-slot:default>图书商城</template>
         </nav-bar>
-        <div class="banner">
-            <img src="~assets/images/1.png" alt="">
+
+        <tab-control v-show="isTabFixed" @tabClick='tabClick' :titles="['热销', '新书', '精选']"></tab-control>
+
+        <div class="wrapper">
+            <div class="content">
+                <div ref="banref">
+                    <div class="banner">
+                        <img src="~assets/images/1.png" alt="">
+                    </div>
+                    <!-- 向子组件传数据 -->
+                    <recommend-view :recommends="recommends"></recommend-view>
+                </div>
+                
+                <tab-control @tabClick='tabClick' :titles="['热销', '新书', '精选']"></tab-control>
+
+                <goods-list :goods="showGoods"></goods-list>
+            </div>
         </div>
-
-        <!-- 向子组件传数据 -->
-        <recommend-view :recommends="recommends"></recommend-view>
-
-        <tab-control @tabClick='tabClick' :titles="['热销', '新书', '精选']"></tab-control>
-
-        <goods-list :goods="showGoods"></goods-list>
+        
 
     </div>
 </template>
@@ -23,13 +32,18 @@ import NavBar from 'components/common/navbar/NavBar'
 import RecommendView from './ChildComps/RecommendView'
 import GoodsList from 'components/content/goods/GoodsList'
 import { getHomeAllData, getHomeGoods } from 'network/home'
-import { ref, reactive, onMounted, computed } from 'vue'
+import { ref, reactive, onMounted, computed, watchEffect, nextTick } from 'vue'
+import BScroll from '@better-scroll/core'
+import Pullup from '@better-scroll/pull-up'
+// 注册插件
+BScroll.use(Pullup)
 
 export default {
     name: 'Home',
 
     setup () {
-
+        let   isTabFixed = ref(false);
+        let   banref     = ref(null);
         const recommends = ref([]);
 
         // 声明商品列表数据模型
@@ -45,6 +59,7 @@ export default {
             return goods[currentType.value].list;
         })
 
+        let bscroll = reactive({});
 
         onMounted( ()=>{
             // console.log(recommends);
@@ -70,20 +85,70 @@ export default {
                 // console.log(res);
                 goods.recommend.list = res.goods.data;
             })
-            console.log(goods);
+            // console.log(goods);
+
+            // 创建BetterScroll对象
+            bscroll = new BScroll('.wrapper', {
+                probeType: 3,    // 0, 1, 2, 3, 3 只要在运动就触发scroll事件
+                click: true,     // 是否允许点击
+                pullUpLoad: true //上拉加载更多， 默认是false
+            });
+
+            //触发滚动事件
+            bscroll.on('scroll', (position)=>{
+                // console.log(position.y)
+                console.log(banref.value.offsetHeight);
+                console.log(-position.y);
+                console.log((-position.y) > banref.value.offsetHeight);
+                isTabFixed.value = (-position.y) > banref.value.offsetHeight;
+            })
+            
+            // 上拉加载数据,触发pullingUp
+            bscroll.on('pullingUp', ()=>{
+                console.log('load more...');
+                // bscroll.refresh();
+                const page = goods[currentType.value].page + 1;
+                getHomeGoods(currentType.value, page).then(res=>{
+                    goods[currentType.value].list.push(...res.goods.data);
+                    goods[currentType.value].page++;
+                })
+                // 完成上拉，等数据请求完成，要将新数据展示出来
+                bscroll.finishPullUp();
+
+                //重新计算高度
+                bscroll.refresh();
+                console.log("contentheight:"+document.querySelector('.content').clientHeight);
+                console.log("当前类型:"+currentType.value+",当前页："+page);
+            })
+
         })
 
         const tabClick = (index)=>{
             let types=['sales', 'new', 'recommend'];
             currentType.value = types[index];
+            nextTick(()=>{
+                // 重新计算高度
+                bscroll &&  bscroll.refresh();
+            })
         }
 
+        // 监听 任何一个变量有变化
+        watchEffect(()=>{
+            nextTick(()=>{
+                // 重新计算高度
+                bscroll && bscroll.refresh();
+            })
+        })
+
         return {
-            tabClick,
+            isTabFixed,
+            banref,
             recommends,
             goods,
-            currentType,
-            showGoods
+            // currentType,
+            showGoods,
+            tabClick,
+            // bscroll
         }
     },
 
@@ -97,10 +162,23 @@ export default {
 </script>
 
 <style scoped>
-.banner img{
-    width: 100%;
-    height: auto;
-    margin-top: 45px;
-}
+    .banner img{
+        width: 100%;
+        height: auto;
+    }
+
+    #home {
+        height:100vh;
+        position: relative;
+    }
+
+    .wrapper {
+        position: absolute;
+        top:45px;
+        bottom:50px;
+        left:0px;
+        right:0px;
+        overflow: hidden;
+    }
 
 </style>
