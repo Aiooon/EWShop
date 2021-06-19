f8cb7ffe2e1042711cad5f33a46a68bcb136d777
ZYao <1307526550@qq.com>
2021-06-19 22:08:23 +0800

    添加 回到顶部

---
+++ src/components/common/backtop/BackTop.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="backTop" @click="backtop">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BackTop",
+    setup(props, {emit}) {
+        return {
+            backtop:() => {
+                emit('bTop');
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.backTop{
+    width: 40px;
+    height: 40px;
+    position: fixed;
+    bottom: 80px;
+    right: 30px;
+    z-index: 10;
+    box-shadow: 3px 3px 3px #888;
+    border-radius: 50%;
+    background: #f6f6f6 url('~assets/images/tag/Uparrow.png') no-repeat center center;
+}
+</style>
