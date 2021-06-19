b3b8d3e6a895fe2a1a0ee7fadd72d28e95b1b051
ZYao <1307526550@qq.com>
2021-06-19 22:08:06 +0800

    引入Vant

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+//引入Vant
+import 'vant/lib/index.css'
+import {Swipe, SwipeItem} from 'vant'
+createApp(App).use(store).use(router)
+        .use(Swipe).use(SwipeItem).mount('#app')
