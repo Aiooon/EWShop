2ae908ef0a8ec2e2005bf5a92150f6ea32046baf
ZYao <1307526550@qq.com>
2021-06-19 22:07:56 +0800

    添加缓存

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <router-view/>
+  <!-- <router-view/> -->
+  <!-- 添加缓存 -->
+  <router-view v-slot="{ Component }">
+      <transition>
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </transition>
+    </router-view>
+
   <div id="nav">
     <router-link class="tab-bar-item" to="/">
       <div class="icon"><i class="iconfont icon-shouye"></i></div>
