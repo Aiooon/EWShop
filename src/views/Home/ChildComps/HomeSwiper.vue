69a222dccc4d4210616f91a96da2504f636e4b15
ZYao <1307526550@qq.com>
2021-06-20 22:25:02 +0800

    更新懒加载

--- src/views/Home/ChildComps/HomeSwiper.vue
+++ src/views/Home/ChildComps/HomeSwiper.vue
@@ -1,7 +1,7 @@
 <template>
     <van-swipe v-if="banners.length" class="my-swipe" :autoplay="3000" indicator-color="white">
         <van-swipe-item v-for="(item, index) in banners" :key="index">
-            <img :src="item.img_url" alt="">
+            <img v-lazy="item.img_url" alt="">
         </van-swipe-item>
     </van-swipe>
 </template>
