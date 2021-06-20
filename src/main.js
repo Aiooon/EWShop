af8fab6dc1c645b7b43a96df4f222811df192cb4
ZYao <1307526550@qq.com>
2021-06-20 22:23:03 +0800

    引入组件

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 //引入Vant
 import 'vant/lib/index.css'
-import {Swipe, SwipeItem} from 'vant'
+import { Tab, Tabs, Swipe, SwipeItem, Lazyload, Badge, Sidebar, SidebarItem, Collapse, CollapseItem, Card, Image as VanImage,Button,Tag } from 'vant'
+
 createApp(App).use(store).use(router)
-        .use(Swipe).use(SwipeItem).mount('#app')
+        .use(Swipe).use(SwipeItem).use(Lazyload,{loading: require('./assets/images/default.png')}).use(Badge).use(Sidebar).use(SidebarItem).use(Collapse).use(CollapseItem).use(Tab).use(Tabs).use(Card).use(VanImage).use(Button).use(Tag)
+        .mount('#app')
