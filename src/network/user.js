b7c86c2debda6e24df49a7e7f412ed2de4ae3dad
ZYao <1307526550@qq.com>
2021-06-21 22:10:41 +0800

    新建用户请求user.js

---
+++ src/network/user.js
@@ -0,0 +1,30 @@
+import {request} from "./request";
+
+export function register(data){
+    return request({
+        url:'/api/auth/register',
+        method:'post',
+        data
+    })
+}
+
+export function login(data){
+    return request({
+        url:'/api/auth/login',
+        method:'post',
+        data
+    })
+}
+
+export function logout(){
+    return request({
+        url:'/api/auth/logout',
+        method:'post'
+    })
+}
+
+export function getUser(){
+    return request({
+        url:'/api/user'
+    })
+}
