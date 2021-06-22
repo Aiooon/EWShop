f390f356a5662dae222a11b83972e6f89282bf5c
ZYao <1307526550@qq.com>
2021-06-22 22:32:00 +0800

    添加 handleAddCart 和 goToCart 方法

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -20,10 +20,8 @@
                 <van-tag plain type="danger">推荐</van-tag>
             </template>
             <template #footer>
-                <!-- <van-button size="warning" @click="handleAddCart">加入购物车</van-button>
-                <van-button size="danger" @click="goToCart">立即购买</van-button> -->
-                <van-button size="warning">加入购物车</van-button>
-                <van-button size="danger">立即购买</van-button>
+                <van-button size="warning" @click="handleAddCart">加入购物车</van-button>
+                <van-button size="danger" @click="goToCart">立即购买</van-button>
             </template>
         </van-card>
 
@@ -46,7 +44,7 @@
 <script>
 import NavBar from 'components/common/navbar/NavBar';
 import GoodsList from "components/content/goods/GoodsList";
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { ref, reactive, onMounted, toRefs } from 'vue';
 import { getDetail } from 'network/detail';
 import { addCart } from 'network/cart';
@@ -59,6 +57,7 @@ export default {
     setup() {
         const route = useRoute();
         const store = useStore();
+        const router = useRouter();
         let id = ref(0);
         let active = ref(0);
         let book = reactive({
@@ -76,12 +75,26 @@ export default {
                 book.comments = res.like_goods;
             })
         })
-        //添加购物车
+
+        // 添加购物车
         const handleAddCart = ()=>{
+            console.log('handleAddCart');
             addCart({goods_id:book.detail.id, num:1}).then(res=>{
                 if(res.status === 204|| res.status === 201){
-                    Toast.success('添加成功')
-                    store.dispatch('updateCart')
+                    Toast.success('添加成功');
+                    store.dispatch('updateCart');
+                }
+            })
+        }
+
+        // 立即购买
+        const goToCart = () =>{
+            console.log('goToCart');
+            addCart({goods_id:book.detail.id, num:1}).then(res=>{
+                if(res.status === 204|| res.status === 201){
+                    Toast.success('添加成功，显示购物车');
+                    store.dispatch('updateCart');
+                    router.push({path: '/shopcart'});
                 }
             })
         }
@@ -90,6 +103,7 @@ export default {
             active,
             ...toRefs(book),
             handleAddCart,
+            goToCart
         }
     },
 
