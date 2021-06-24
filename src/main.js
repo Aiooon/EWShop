a991697d99df21f96efc5dd1279b61d59154396b
ZYao <1307526550@qq.com>
2021-06-24 10:51:50 +0800

    添加 Checkbox, CheckboxGroup, SubmitBar, SwipeCell, Stepper

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import store from './store'
 //引入Vant
 import 'vant/lib/index.css'
-import { Tab, Tabs, Swipe, SwipeItem, Lazyload, Badge, Sidebar, SidebarItem, Collapse, CollapseItem, Card, Image as VanImage,Button,Tag, Form, Field } from 'vant'
+import { Tab, Tabs, Swipe, SwipeItem, Lazyload, Badge, Sidebar, SidebarItem, Collapse, CollapseItem, Card, Image as VanImage,Button,Tag, Form, Field, Checkbox, CheckboxGroup, SubmitBar, SwipeCell, Stepper} from 'vant'
 
 createApp(App).use(store).use(router)
-        .use(Swipe).use(SwipeItem).use(Lazyload,{loading: require('./assets/images/default.png')}).use(Badge).use(Sidebar).use(SidebarItem).use(Collapse).use(CollapseItem).use(Tab).use(Tabs).use(Card).use(VanImage).use(Button).use(Tag).use(Form).use(Field)
+        .use(Swipe).use(SwipeItem).use(Lazyload,{loading: require('./assets/images/default.png')}).use(Badge).use(Sidebar).use(SidebarItem).use(Collapse).use(CollapseItem).use(Tab).use(Tabs).use(Card).use(VanImage).use(Button).use(Tag).use(Form).use(Field).use(Checkbox).use(CheckboxGroup).use(SubmitBar).use(Stepper).use(SwipeCell)
         .mount('#app')
