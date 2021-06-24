145ea27c5b7c3198d09b9b3dac3aa1bd1a297862
ZYao <1307526550@qq.com>
2021-06-24 22:09:05 +0800

    添加组件

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import store from './store'
 //引入Vant
 import 'vant/lib/index.css'
-import { Tab, Tabs, Swipe, SwipeItem, Lazyload, Badge, Sidebar, SidebarItem, Collapse, CollapseItem, Card, Image as VanImage,Button,Tag, Form, Field, Checkbox, CheckboxGroup, SubmitBar, SwipeCell, Stepper} from 'vant'
+import { Icon, Tab, Tabs, Swipe, SwipeItem, Lazyload, Badge, Sidebar, SidebarItem, Collapse, CollapseItem, Card, Image as VanImage,Button,Tag, Form, Field, Checkbox, CheckboxGroup, SubmitBar, SwipeCell, Stepper, AddressEdit, AddressList, Popup,Grid, GridItem } from 'vant'
 
 createApp(App).use(store).use(router)
-        .use(Swipe).use(SwipeItem).use(Lazyload,{loading: require('./assets/images/default.png')}).use(Badge).use(Sidebar).use(SidebarItem).use(Collapse).use(CollapseItem).use(Tab).use(Tabs).use(Card).use(VanImage).use(Button).use(Tag).use(Form).use(Field).use(Checkbox).use(CheckboxGroup).use(SubmitBar).use(Stepper).use(SwipeCell)
+        .use(Swipe).use(SwipeItem).use(Lazyload,{loading: require('./assets/images/default.png')}).use(Badge).use(Sidebar).use(SidebarItem).use(Collapse).use(CollapseItem).use(Tab).use(Tabs).use(Card).use(VanImage).use(Button).use(Tag).use(Form).use(Field).use(Checkbox).use(CheckboxGroup).use(SubmitBar).use(Stepper).use(SwipeCell).use(Icon).use(AddressEdit).use(AddressList).use(Popup).use(Grid).use(GridItem)
         .mount('#app')
